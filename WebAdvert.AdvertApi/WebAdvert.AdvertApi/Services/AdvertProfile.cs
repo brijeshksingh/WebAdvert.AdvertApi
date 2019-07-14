4c081fa253c0051d0859f9d922c83cd9613150a9
Ajit Singh <ajit51081@gmail.com>
2019-07-14 12:43:29 +0530

    commit all webapi and model

---
+++ WebAdvert.AdvertApi/WebAdvert.AdvertApi/Services/AdvertProfile.cs
@@ -0,0 +1,17 @@
+﻿using AdvertApi.Model;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.AdvertApi.Services
+{
+    public class AdvertProfile : Profile
+    {
+        public AdvertProfile()
+        {
+            CreateMap<AdvertModel, AdvertDbModel>();
+        }
+    }
+}
---
+++ WebAdvert.AdvertApi/WebAdvert.AdvertApi/Services/IAdvertStorageService.cs
@@ -0,0 +1,14 @@
+﻿using AdvertApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAdvert.AdvertApi.Services
+{
+   public interface IAdvertStorageService
+    {
+        Task<string> Add(AdvertModel model);
+        Task Confirm(ConfirmAdvertModel model);
+    }
+}
